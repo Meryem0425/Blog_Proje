338777abb3748f7d2fafa546069622c7b3091403
Meryem Yoncalık <117303770+Meryem0425@users.noreply.github.com>
2024-03-12 16:48:39 +0300

    statik olarak herşey bittti

--- Blog/urls.py
+++ Blog/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path('',views.index, name="home"),
     path('about/',views.about, name="about"),
     path('contact/',views.contact, name="contact"),
+    path('celsusAr/',views.celsusar, name="celsusAr"),
+    path('hacettepeAr/',views.hacettepear, name="hacettepear"),
+    path('blogs/',views.blogs, name="blogs"),
    
     ]
--- Blog/views.py
+++ Blog/views.py
@@ -9,3 +9,11 @@ def about(request):
 
 def contact(request):
     return render(request,'blog/contact.html')
+
+def celsusar(request):
+    return render(request,'blog/celsus_Ar.html')
+
+def hacettepear(request):
+    return render(request,'blog/hacettepe_Ar.html')
+def blogs(request):
+    return render(request,'blog/blogs.html')
